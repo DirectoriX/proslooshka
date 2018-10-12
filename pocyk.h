ab8d44c2b8fd06f5d7a40ead4d3f5d5dfcc149eb
Dixaba <dixaba@yandex.ru>
2018-10-12 14:49:01 +0300

    Старые варианты проекта - без БД

---
+++ pocyk.h
@@ -0,0 +1,36 @@
+﻿#ifndef POCYK_H
+#define POCYK_H
+
+#include <array>
+#include <iostream>
+
+class Pocyk {
+  public:
+    Pocyk(int category, int number);
+    ~Pocyk();
+
+    void toggleActivity(); // Переключение статуса активности
+    float getK(); // Получение коэффициента
+    void skip(); // Повезло и прослушали не его
+    void resetCalls(); // Сброс звонков
+    void addCallsDno(); // Добавление прослушиваний новичкам
+    void call(); // Прослушали :-(
+
+    friend bool operator<(const Pocyk &left, const Pocyk &right); // Нужно для сортировки
+
+    static void init(float delta, int cat1, int cat2, int cat3); // Первоначальная настройка
+
+    static void printStat(); // Вывод небольшой статистики
+
+  private:
+    int calls; // Количество оставшихся звонков
+    int category; // Категория оператора (0 - новичок, 2 - Скрипаль)
+    int number; // Номер оператора внутри категории. Только для вывода на экран
+    bool active; // Флаг активности
+    float multiplier; // Множитель для вычисления коэффициента
+    float k; // Сам коэффициент
+
+    inline void calc(); // Вычисление коэффициента
+};
+
+#endif // POCYK_H
