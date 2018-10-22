1c80d000ad779bb316fb3934564e8754981e6aeb
Dixaba <dixaba@yandex.ru>
2018-10-22 14:39:12 +0300

    Оставил только распределение звонков между ОККшниками

--- db.cpp
+++ db.cpp
@@ -1,6 +1,4 @@
 #include <algorithm>
-#include <array>
-#include <climits>
 #include <iostream>
 #include <random>
 #include <sqlite3.h>
@@ -11,7 +9,6 @@ using namespace std;
 
 const bool debug = true; // FIXME
 const bool printOut = true; // WARNING
-const int maxWeeks = 0;
 
 // Куча настроек категорий
 const int cat0Calls = 8;
@@ -52,11 +49,11 @@ struct OKK
 };
 
 // Пара вспомогательных переменных
-int id = 0;
 char *zErrMsg = nullptr;
 int rc;
 string req;
 
+// Проверяем запрос к БД на ошибки
 void check()
 {
   if (rc != SQLITE_OK) // Проверяем на ошибки
@@ -77,26 +74,6 @@ int zeroCallback(void *NotUsed, int argc, char **argv, char **azColName)
     }
 
   cout << endl;
-  return 0;
-}
-
-// Возвращает rowid
-int rowidCallback(void *rowid, int argc, char **argv, char **azColName)
-{
-  if (printOut)
-    { zeroCallback(rowid, argc, argv, azColName); }
-
-  *static_cast<int *>(rowid) = std::stoi(argv[0]);
-  return 0;
-}
-
-// Возвращает count. Чем отличается от предыдущего? Ничем
-int countCallback(void *count, int argc, char **argv, char **azColName)
-{
-  if (debug && printOut)
-    { zeroCallback(count, argc, argv, azColName); }
-
-  *static_cast<int *>(count) = std::stoi(argv[0]);
   return 0;
 }
 
@@ -136,18 +113,6 @@ int OKKCallback(void *OKKs, int argc, char **argv, char **azColName)
   return 1;
 }
 
-// Создаёт таблицу поцыков
-void createTablePocyks(sqlite3 *db)
-{
-  req = "DROP TABLE IF EXISTS pocyks; CREATE TABLE pocyks (cat INT, name TEXT, callsRemain INT, mul FLOAT, active INT);";
-
-  if (debug)
-    { cout << req << endl; }
-
-  rc = sqlite3_exec(db, req.data(), zeroCallback, nullptr, &zErrMsg);
-  check();
-}
-
 // Создаёт таблицу ОККшников
 void createTableOKKs(sqlite3 *db)
 {
@@ -161,24 +126,6 @@ void createTableOKKs(sqlite3 *db)
     }
 
   req += ");";
-
-  if (debug)
-    { cout << req << endl; }
-
-  rc = sqlite3_exec(db, req.data(), zeroCallback, nullptr, &zErrMsg);
-  check();
-}
-
-// Добавляет поцыка
-void insertPocyk(sqlite3 *db, int cat, int number, int pocyks)
-{
-  req = "INSERT INTO pocyks (cat, name, callsRemain, mul, active) VALUES (";
-  req += std::to_string(cat);
-  req += ", 'Поцык №";
-  req += std::to_string(number);
-  req += "', ";
-  req += std::to_string(pocyks);
-  req += ", 1, 1);";
 
   if (debug)
     { cout << req << endl; }
@@ -212,40 +159,6 @@ void insertOKK(sqlite3 *db, const string &name, int code)
     }
 
   req += ");";
-
-  if (debug)
-    { cout << req << endl; }
-
-  rc = sqlite3_exec(db, req.data(), zeroCallback, nullptr, &zErrMsg);
-  check();
-}
-
-// Выбираем поцыка для звонка
-void call(sqlite3 *db)
-{
-  // Получаем поцыка
-  int rowid;
-  req = "SELECT rowid, cat, name FROM pocyks WHERE active = 1 ORDER BY callsRemain*mul DESC LIMIT 1;";
-
-  if (debug)
-    { cout << req << endl; }
-
-  rc = sqlite3_exec(db, req.data(), rowidCallback, &rowid, &zErrMsg);
-  check();
-  // Обновляем этого поцыка
-  req = "UPDATE pocyks SET callsRemain = callsRemain-1, mul=mul/2 WHERE rowid=";
-  req += std::to_string(rowid);
-  req += ";";
-
-  if (debug)
-    { cout << req << endl; }
-
-  rc = sqlite3_exec(db, req.data(), zeroCallback, nullptr, &zErrMsg);
-  check();
-  // Обновляем других поцыков
-  req = "UPDATE pocyks SET mul = mul+1.0+1.0*random()/9223372036854775807 WHERE rowid!=";
-  req += std::to_string(rowid);
-  req += ";";
 
   if (debug)
     { cout << req << endl; }
@@ -422,78 +335,6 @@ void planOKK(sqlite3 *db)
     }
 }
 
-// Сброс звонков поцыков в конце недели
-void resetCalls(sqlite3 *db)
-{
-  req = "UPDATE pocyks SET callsRemain = ";
-  req += std::to_string(cat0Calls);
-  req += " WHERE cat=0;";
-
-  if (debug)
-    { cout << req << endl; }
-
-  rc = sqlite3_exec(db, req.data(), zeroCallback, nullptr, &zErrMsg);
-  check();
-  req = "UPDATE pocyks SET callsRemain = ";
-  req += std::to_string(cat1Calls);
-  req += " WHERE cat=1;";
-
-  if (debug)
-    { cout << req << endl; }
-
-  rc = sqlite3_exec(db, req.data(), zeroCallback, nullptr, &zErrMsg);
-  check();
-  req = "UPDATE pocyks SET callsRemain = ";
-  req += std::to_string(cat2Calls);
-  req += " WHERE cat=2;";
-
-  if (debug)
-    { cout << req << endl; }
-
-  rc = sqlite3_exec(db, req.data(), zeroCallback, nullptr, &zErrMsg);
-  check();
-}
-
-// Накидываем звонков начинающим поцыкам
-void addCallsDno(sqlite3 *db)
-{
-  req = "UPDATE pocyks SET callsRemain = ";
-  req += std::to_string(cat0Calls);
-  req += " WHERE cat=0;";
-
-  if (debug)
-    { cout << req << endl; }
-
-  rc = sqlite3_exec(db, req.data(), zeroCallback, nullptr, &zErrMsg);
-  check();
-}
-
-// Проверяем, есть ли ещё поцыки со звонками
-bool callsRemain(sqlite3 *db)
-{
-  int count;
-  req = "SELECT COUNT(*) FROM pocyks WHERE callsRemain>0 AND active=1;";
-
-  if (debug)
-    { cout << req << endl; }
-
-  rc = sqlite3_exec(db, req.data(), countCallback, &count, &zErrMsg);
-  check();
-  return count > 0;
-}
-
-// Переключаем статус активности поцыка случайным образом
-void toggleActivityRandom(sqlite3 *db)
-{
-  req = "UPDATE pocyks SET active = 1-active WHERE rowid=(SELECT rowid FROM pocyks ORDER BY random() LIMIT 1);";
-
-  if (debug)
-    { cout << req << endl; }
-
-  rc = sqlite3_exec(db, req.data(), zeroCallback, nullptr, &zErrMsg);
-  check();
-}
-
 // Подключаемся к БД
 sqlite3 *connect(bool inMemory)
 {
@@ -537,26 +378,6 @@ int main()
 
   sqlite3 *db = connect(false);
   cout << endl << endl;
-  createTablePocyks(db); // Создаём таблицу поцыков
-  cout << endl << "add pocyks" << endl << endl;
-  //  Добавляем поцыков в таблицу в три захода
-  {
-    for (int i = 0; i < cat0Count; i++)
-      {
-        insertPocyk(db, 0, i, cat0Calls);
-      }
-
-    for (int i = 0; i < cat1Count; i++)
-      {
-        insertPocyk(db, 1, i, cat1Calls);
-      }
-
-    for (int i = 0; i < cat2Count; i++)
-      {
-        insertPocyk(db, 2, i, cat2Calls);
-      }
-  }
-  cout << endl << endl;
   createTableOKKs(db); // Создаём таблицу ОКК
   cout << endl << "add OKKs" << endl << endl;
   //  Добавляем OKK в таблицу в три захода
@@ -572,31 +393,6 @@ int main()
   }
   cout << endl << "planirovanie OKK" << endl << endl;
   planOKK(db);
-
-  for (int week = 0; week < maxWeeks; week++) // Много недель подряд...
-    {
-      cout << "----- Week " << week << " -----" << endl << endl;
-
-      for (int i = 0; i < totalCalls; i++) // ... столько раз, сколько положено...
-        {
-          if (1.0 * rand() / RAND_MAX < 0.33) // С какой-то вероятностью поцык может уйти в отпуск или вернуться
-            {
-              toggleActivityRandom(db);
-            }
-
-          if (!callsRemain(db)) // Если больше нет доступных для прослушивания поцыков
-            {
-              addCallsDno(db); // Накидываем прослушек доньям
-            }
-
-          call(db);
-        }
-
-      // Неделя закончилась - всем сбрасываем количество прослушиваний
-      resetCalls(db);
-      cout << endl << endl; // Разделяем недели пустой строкой
-    }
-
   // Закрываем базу нафиг, хоть она и в памяти
   sqlite3_close(db);
   return 0;
